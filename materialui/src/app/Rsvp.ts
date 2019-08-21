7479ae51743df9cbeb429a3e30e5870186f44388
unknown <bunnyppl@hotmail.com>
2019-08-21 22:14:06 +0800

    added material ui

---
+++ materialui/src/app/Rsvp.ts
@@ -0,0 +1,10 @@
+export class Rsvp {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: string,
+        public attending?: string
+    ) {
+
+    }
+}
--- materialui/src/app/app.module.ts
+++ materialui/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
